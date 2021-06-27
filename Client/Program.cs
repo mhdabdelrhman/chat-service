5553d2fc2315141449aa3fb21ced2a21b1c58152
MohammedAbdAlrehman <moh.3bd.rhman@gmail.com>
2021-06-27 14:26:57 +0300

    Re-arrange files and projects

--- Client/Program.cs
+++ Client/Program.cs
@@ -6,7 +6,7 @@ namespace Chat.Client
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Client Started.");
         }
     }
 }
